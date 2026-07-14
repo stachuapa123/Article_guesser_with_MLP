44193b10e4a6ec80bbde5bcb4d271009f2cae03c
Stanislaw Apanasiewicz <mmapanasiewicz@gmail.com>
2026-07-14 13:47:19 +0200

    lstm model made and train function taken from homlp

--- src/rnn_model.py
+++ src/rnn_model.py
@@ -1,6 +1,9 @@
 import torch 
 from torch.utils.data import Dataset
+import torch.nn as nn
+import pipelines as P
 
+V_len = len(P.letters)
 class ArticleDataset(Dataset):
     def __init__(self,X,y):
         self.X = X
@@ -12,5 +15,64 @@ class ArticleDataset(Dataset):
     def __getitem__(self, idx):
         if idx >= len(self):
             raise IndexError("dataset index out of range")
-          # 1st index after window
         return self.X[idx], self.y[idx]
+    
+class ArticleLSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes=3):
+        super().__init__()
+        self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size, batch_first=True)
+        self.output = nn.Linear(hidden_size, num_classes)
+
+    def forward(self, x, lengths):
+        packed = nn.utils.rnn.pack_padded_sequence(x, lengths, batch_first=True, enforce_sorted=False)
+        tick_output, (hidden, cell) = self.lstm(packed)
+        out = self.output(hidden[-1])
+        return out
+    
+
+import torchmetrics
+
+def evaluate_tm(device, model, data_loader, metric):
+    model.eval()
+    metric.reset()
+    with torch.no_grad():
+        for X_batch, y_batch in data_loader:
+            X_batch, y_batch = X_batch.to(device), y_batch.to(device)
+            y_pred = model(X_batch)
+            metric.update(y_pred, y_batch)
+    return metric.compute()
+
+def train(device, model, optimizer, loss_fn, metric, train_loader, valid_loader,
+          n_epochs, patience=2, factor=0.5, epoch_callback=None):
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+        optimizer, mode="max", patience=patience, factor=factor)
+    history = {"train_losses": [], "train_metrics": [], "valid_metrics": []}
+    for epoch in range(n_epochs):
+        total_loss = 0.0
+        metric.reset()
+        model.train()
+        if epoch_callback is not None:
+            epoch_callback(model, epoch)
+        for index, (X_batch, y_batch) in enumerate(train_loader):
+            X_batch, y_batch = X_batch.to(device), y_batch.to(device)
+            y_pred = model(X_batch)
+            loss = loss_fn(y_pred, y_batch)
+            total_loss += loss.item()
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+            metric.update(y_pred, y_batch)
+            train_metric = metric.compute().item()
+            print(f"\rBatch {index + 1}/{len(train_loader)}", end="")
+            print(f", loss={total_loss/(index+1):.4f}", end="")
+            print(f", {train_metric=:.2%}", end="")
+        history["train_losses"].append(total_loss / len(train_loader))
+        history["train_metrics"].append(train_metric)
+        val_metric = evaluate_tm(model, valid_loader, metric).item()
+        history["valid_metrics"].append(val_metric)
+        scheduler.step(val_metric)
+        print(f"\rEpoch {epoch + 1}/{n_epochs},                      "
+              f"train loss: {history['train_losses'][-1]:.4f}, "
+              f"train metric: {history['train_metrics'][-1]:.2%}, "
+              f"valid metric: {history['valid_metrics'][-1]:.2%}")
+    return history
